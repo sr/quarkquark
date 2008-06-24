883d5656a355a5cf20f93a33a5a9af31a900253e
Simon Rozet <simon@rozet.name>
2008-06-24 18:07:24 +0200

    example of how I want the DSL to be

---
+++ atom_pub_server_example.rb
@@ -0,0 +1,32 @@
+require File.dirname(__FILE__) + '/lib/atom_pub_server'
+
+atom_pub_server = server do
+  # same as for author
+  store :memory,  :adapter  => 'sqlite3',
+                  :database => 'entries.sqlite3'
+  # register a default author for all the collections
+  # if you want to assigns a different author to a collection,
+  # just use `author' in the collection block
+  author :name  => 'Simon Rozet',
+         :email => 'simon@rozet.name'
+
+  # it could be done by using rack's 'use' instead...
+  # also, how about auth per-collection?
+  authenticate :login => 'simon',
+               :password => 'foobarspam'
+
+  # which one is best? both?
+  collection do
+    title 'Articles'
+    author :name => 'John Smith'
+  end
+
+  # will use global author and global authentication
+  collection :linking
+  collection :foo, 'Foo Bar Spam'
+  collection 'My Collection Title'
+
+  collection :photo do
+    title 'My not-so-good photography'
+  end
+end
