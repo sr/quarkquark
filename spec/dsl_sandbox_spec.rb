15bed00037f180e8c53c80df86c9619e89e31fe4
Simon Rozet <simon@rozet.name>
2008-06-26 00:49:49 +0200

    better spec and fixing the pending examples

--- spec/dsl_sandbox_spec.rb
+++ spec/dsl_sandbox_spec.rb
@@ -10,36 +10,35 @@ describe DslSandbox do
 
   describe 'collection' do
     before(:each) do
-      @collection_proxy = mock(DslSandbox::CollectionProxy)
-      DslSandbox::CollectionProxy.should_receive(:new).and_return(@collection_proxy)
+      @collection_proxy = DslSandbox::CollectionProxy.new
+      DslSandbox::CollectionProxy.stub!(:new).and_return(@collection_proxy)
     end
 
     it 'accepts to only be given an identifier and convert it to a title' do
-      @collection_proxy.should_receive(:title=).with('Linking')
-      @collection_proxy.should_receive(:identifier=).with(:linking)
-      @store_proxy.collection(:linking)
+      collection = @store_proxy.collection(:linking)
+      collection.title.to_s.should == 'Linking'
+      collection.identifier.should == :linking
     end
 
     it 'accepts to be given both an identifier and a title (in that order)' do
-      @collection_proxy.should_receive(:title=).with('Foo Bar Articles')
-      @collection_proxy.should_receive(:identifier=).with(:articles)
-      @store_proxy.collection(:articles, 'Foo Bar Articles')
+      collection = @store_proxy.collection(:articles, 'Foo Bar Articles')
+      collection.title.to_s.should == 'Foo Bar Articles'
+      collection.identifier.should == :articles
     end
 
-    it 'accepts a block with no arguments' do
-      pending 'needs to figure out how to stub CollectionProxy properly'
-      @collection_proxy.should_receive(:title=).with('foo bar')
-      @collection_proxy.should_receive(:identifier=).with(:foo)
-      @store_proxy.collection { title 'foo bar' }
+    it 'accepts no arguments with a block yielding title and create an identifier from it' do
+      collection = @store_proxy.collection { title 'bar' }
+      collection.title.to_s.should == 'bar'
+      collection.identifier.should == :bar
     end
 
     it 'accepts a block with the identifier as only argument' do
-      pending 'needs to figure out how to stub CollectionProxy properly'
-      @collection_proxy.should_receive(:identifier=).with(:foofoo)
-      @store_proxy.collection(:foofoo) { title 'Foo Bar' }
+      collection = @store_proxy.collection(:foofoo) { title 'Foo Bar' }
+      collection.title.to_s.should == 'Foo Bar'
+      collection.identifier.should == :foofoo
     end
 
-    it 'accepts a block an author' do
+    it 'accepts a block yielding an author' do
       pending 'needs to figure out how to stub CollectionProxy properly'
     end
   end
