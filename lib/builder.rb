dacd2c59fc8da0968741aa3757afb20a206c7799
Simon Rozet <simon@rozet.name>
2008-07-03 17:53:14 +0200

    basic spec

--- lib/builder.rb
+++ lib/builder.rb
@@ -1,4 +1,12 @@
 module AtomPub
   class Builder
+    def initialize(server_proxy)
+      @proxy = server_proxy
+      build
+    end
+
+    def build
+      @proxy.store
+    end
   end
 end
--- spec/builder_spec.rb
+++ spec/builder_spec.rb
@@ -1,5 +1,20 @@
 require File.dirname(__FILE__) + '/spec_helper' 
+require 'dsl'
 require File.dirname(__FILE__) + '/../lib/builder' # conflict with the gem builder
 
 describe AtomPub::Builder do
+  before(:each) do
+    @proxy = AtomPub::DSL::ServerProxy.new
+  end
+
+  describe 'When initializing store' do
+    before(:each) do
+      @store = mock('store')
+    end
+
+    it 'initialize the store with given options' do
+      @proxy.should_receive(:store).and_return(@store)
+      @server = AtomPub::Builder.new(@proxy)
+    end
+  end
 end
